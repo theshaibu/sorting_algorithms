b5b3d94be7c9d191cb231b3c024052dc3f4f0376
shaibu sulleyman <shaibusulleyman3@gmail.com>
2024-01-27 06:17:13 -0800

    function that sorts a doubly linked list of integers in ascending order using the Cocktail shaker sort algorithm

---
+++ 101-cocktail_sort_list.c
@@ -0,0 +1,96 @@
+#include "sort.h"
+
+/**
+ * list_len - Returns the length of a doubly linked list.
+ * @list: Head of the list.
+ *
+ * Return: Length of the list.
+ */
+size_t list_len(listint_t *list)
+{
+    size_t len = 0;
+
+    while (list)
+    {
+        len++;
+        list = list->next;
+    }
+    return (len);
+}
+
+/**
+ * switch_nodes - Swaps nodes at pointer p with the following node.
+ * @list: Pointer to the head of the list.
+ * @p: Pointer to the node.
+ */
+void switch_nodes(listint_t **list, listint_t **p)
+{
+    listint_t *one, *two, *three, *four;
+
+    one = (*p)->prev;
+    two = *p;
+    three = (*p)->next;
+    four = (*p)->next->next;
+    two->next = four;
+    if (four)
+        four->prev = two;
+    three->next = two;
+    three->prev = two->prev;
+    if (one)
+        one->next = three;
+    else
+        *list = three;
+    two->prev = three;
+    *p = three;
+}
+
+/**
+ * cocktail_sort_list - Sorts a doubly linked list using the
+ *                     Cocktail Shaker Sort algorithm.
+ * @list: Pointer to the head of the list.
+ */
+void cocktail_sort_list(listint_t **list)
+{
+    listint_t *p;
+    int sorted = 0;
+
+    if (!list || !*list || list_len(*list) < 2)
+        return;
+
+    p = *list;
+
+    while (!sorted)
+    {
+        sorted = 1;
+        while (p->next)
+        {
+            if (p->n > p->next->n)
+            {
+                sorted = 0;
+                switch_nodes(list, &p);
+                print_list(*list);
+            }
+            else
+                p = p->next;
+        }
+
+        if (sorted)
+            break;
+
+        p = p->prev;
+
+        while (p->prev)
+        {
+            if (p->n < p->prev->n)
+            {
+                sorted = 0;
+                p = p->prev;
+                switch_nodes(list, &p);
+                print_list(*list);
+            }
+            else
+                p = p->prev;
+        }
+    }
+}
+
