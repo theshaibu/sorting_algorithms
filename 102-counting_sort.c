46bb664d92da154c85ab2e93779ad08cebe27ddc
shaibu sulleyman <shaibusulleyman3@gmail.com>
2024-01-27 06:57:13 -0800

    function that sorts an array of integers in ascending order using the Counting sort algorithm

--- 102-counting_sort.c
+++ 102-counting_sort.c
@@ -1,57 +1,44 @@
 #include "sort.h"
 
 /**
- * counting_sort - Sorts an array with the Counting sort algorithm.
- * @array: Array to sort.
- * @size: Size of the array.
+ * counting_sort - sorts an array with the Counting sort algorithm
+ * @array: array to sort
+ * @size: size of the array
  */
 void counting_sort(int *array, size_t size)
 {
-    if (size < 2 || array == NULL)
-        return;
+	int *count_arr, *out_arr, max, num, j, l;
+	size_t i, k, m, n;
 
-    int *count_arr, *out_arr, max, num;
-    size_t i, k, m, n;
+	if (size < 2)
+		return;
 
-    max = array[0];
-    for (i = 1; i < size; i++)
-        if (array[i] > max)
-            max = array[i];
+	max = array[0];
+	for (i = 1; i < size; i++)
+		if (array[i] > max)
+			max = array[i];
 
-    count_arr = malloc(sizeof(int) * (max + 1));
-    out_arr = malloc(sizeof(int) * size);
+	count_arr = malloc(sizeof(size_t) * (max + 1));
+	out_arr = malloc(sizeof(int) * size);
 
-    if (count_arr == NULL || out_arr == NULL)
-    {
-        free(count_arr);
-        free(out_arr);
-        return;
-    }
+	for (j = 0; j <= max; j++)
+		count_arr[j] = 0;
+	for (k = 0; k < size; k++)
+	{
+		num = array[k];
+		count_arr[num] += 1;
+	}
+	for (l = 1; l <= max; l++)
+		count_arr[l] += count_arr[l - 1];
+	print_array(count_arr, max + 1);
+	for (m = 0; m < size; m++)
+	{
+		out_arr[count_arr[array[m]] - 1] = array[m];
+		count_arr[array[m]]--;
+	}
+	for (n = 0; n < size; n++)
+		array[n] = out_arr[n];
 
-    for (k = 0; k <= max; k++)
-        count_arr[k] = 0;
-
-    for (k = 0; k < size; k++)
-    {
-        num = array[k];
-        count_arr[num] += 1;
-    }
-
-    for (k = 1; k <= max; k++)
-        count_arr[k] += count_arr[k - 1];
-
-    print_array(count_arr, max + 1);
-
-    for (m = 0; m < size; m++)
-    {
-        out_arr[count_arr[array[m]] - 1] = array[m];
-        count_arr[array[m]]--;
-    }
-
-    for (n = 0; n < size; n++)
-        array[n] = out_arr[n];
-
-    free(count_arr);
-    free(out_arr);
+	free(count_arr);
+	free(out_arr);
 }
-
--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -1,36 +1,31 @@
 #include "sort.h"
 
 /**
- * selection_sort - Sorts an array using the selection sort algorithm.
- * @array: Array to sort.
- * @size: Size of the array.
+ * selection_sort - sorts an array following the selection sort algorithm
+ * @array: array to sort
+ * @size: size of the array;
  */
 void selection_sort(int *array, size_t size)
 {
-    if (size < 2 || array == NULL)
-        return;
+	unsigned int i, j, min;
 
-    unsigned int i, j, min;
-    int tmp;
+	register int tmp;
 
-    for (i = 0; i < size - 1; i++)
-    {
-        min = i;
+	if (size < 2)
+		return;
 
-        for (j = i + 1; j < size; j++)
-        {
-            if (array[j] < array[min])
-                min = j;
-        }
-
-        if (min != i)
-        {
-            tmp = array[i];
-            array[i] = array[min];
-            array[min] = tmp;
-
-            print_array(array, size);
-        }
-    }
+	for (i = 0; i < size; i++)
+	{
+		min = i;
+		for (j = i + 1; j < size; j++)
+		{
+			if (array[j] < array[min])
+				min = j;
+		}
+		tmp = array[i];
+		array[i] = array[min];
+		array[min] = tmp;
+		if (i != min)
+			print_array(array, size);
+	}
 }
-
